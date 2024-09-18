423e83957150d94253e87e7efb3239aab9e810fc
Ravid Efroni <ravidefroni@gmail.com>
2024-09-18 15:18:36 +0200

    Add functionality to the inputs

--- src/App.js
+++ src/App.js
@@ -4,71 +4,89 @@ import { useState } from 'react';
 import logo from './logo.svg';
 
 export default function App() {
-  const [data, setData] = useState([]);
+  const [memeGen, setMemeGen] = useState([]);
   // const [error, setError] = useState();
   const [loading, setLoading] = useState();
   const [userInputUpper, setUserInputUpper] = useState('');
   const [userInputLow, setUserInputLow] = useState('');
 
-  const fetchTemplates = () => {
-    axios
-      .get('https://api.memegen.link/templates/')
-      .then((response) => {
-        // Step 3: Store the fetched data in state
-        setData(response.data);
-      })
-      .catch((error) => {
-        console.error('Error fetching templates:', error);
-      });
-  };
-
-  fetchTemplates();
+  const urlImages = 'https://api.memegen.link/images/';
 
   return (
     <div className="App">
-      <div>
-        Hello again
-        {data.map((dataItem) => {
-          return (
-            <div key={`my data item is-${dataItem.id}`}>
-              <img src={dataItem.blank} alt={dataItem.name} />
-            </div>
-          );
-        })}
-      </div>
+      <div>{/*   */}</div>
       <div style={{ marginTop: '100px' }}>
-        <h1>Hello world </h1>
+        <h1>This is an H1 </h1>
 
         <form
           onSubmit={(event) => {
             event.preventDefault();
-            setUserInputUpper(userInputUpper);
-            setUserInputLow(userInputLow);
           }}
         >
-          <label htmlFor="upperField">New Input</label>
+          <label htmlFor="Meme template">meme gen first </label>
+          <input
+            value={memeGen}
+            id="Meme template"
+            onChange={(event) => setMemeGen(event.currentTarget.value)}
+          />
+          <br />
+          <br />
+          <br />
+          <label htmlFor="Top text">Upper Input</label>
           <input
             value={userInputUpper}
-            id="upperField"
+            id="Top text"
             onChange={(event) => setUserInputUpper(event.currentTarget.value)}
           />
           <br />
 
           <br />
-          <label htmlFor="lowerField">New Input</label>
+          <label htmlFor="Bottom text">Lower Input</label>
           <input
             value={userInputLow}
-            id="lowerField"
+            id="Bottom text"
             onChange={(event) => setUserInputLow(event.currentTarget.value)}
           />
           <br />
           <br />
 
-          <button>Submit</button>
+          <button>Generate meme</button>
+          <br />
+          <br />
+          <img
+            htmlFor=""
+            style={{ height: '150px' }}
+            src={`${urlImages}${memeGen}/${userInputUpper}/${userInputLow}.jpg`}
+            alt="meme-image"
+          />
         </form>
 
-        <div></div>
+        {/* <div>
+          Hello again
+          {image.map((imageItem) => {
+            return (
+              <div key={`my data item is-${imageItem.id}`}>
+                <img src={imageItem.blank} alt={imageItem.name} />
+              </div>
+            );
+          })}
+        </div> */}
       </div>
     </div>
   );
 }
+
+// fetch the data link and put it in the data array
+/* const fetchTemplates = () => {
+    axios
+      .get('https://api.memegen.link/templates/')
+      .then((response) => {
+        // Step 3: Store the fetched data in state
+        setImage(response.data.slice(0, 10));
+      })
+      .catch((error) => {
+        console.error('Error fetching templates:', error);
+      });
+  }; */
+
+//fetchTemplates();
