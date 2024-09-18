1a867353073ef0bac9eed7c1039fb986f513156a
Ravid Efroni <ravidefroni@gmail.com>
2024-09-18 09:12:32 +0200

    Add mapping to get the images

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ export default function App() {
       .then((response) => {
         // Step 3: Store the fetched data in state
         setData(response.data);
-        //console.log(response.data[0].url);
       })
       .catch((error) => {
         console.error('Error fetching templates:', error);
@@ -27,6 +26,16 @@ export default function App() {
 
   return (
     <div className="App">
+      <div>
+        Hello again
+        {data.map((dataItem) => {
+          return (
+            <div key={`my data item is-${dataItem.id}`}>
+              <img src={dataItem.blank} alt={dataItem.name} />
+            </div>
+          );
+        })}
+      </div>
       <div style={{ marginTop: '100px' }}>
         <h1>Hello world </h1>
 
