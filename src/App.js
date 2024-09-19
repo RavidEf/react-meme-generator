645bb143a62c7221a7ac461647675ff700bb4242
Ravid Efroni <ravidefroni@gmail.com>
2024-09-19 13:00:41 +0200

    Add correct lables text

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import axios from 'axios';
 import { useState } from 'react';
+import styles from './app.scss';
+// import styles from './index.css';
 import logo from './logo.svg';
 
 export default function App() {
@@ -13,77 +15,56 @@ export default function App() {
   const urlImages = 'https://api.memegen.link/images/';
 
   return (
-    <div className="App">
+    <div id="App">
       <div style={{ marginTop: '100px' }}>
         <h1>This is an H1 </h1>
 
-        <form>
-          <label
-            htmlFor="Meme template"
-            onSubmit={(event) => event.preventDefault()}
-          >
-            meme gen first{' '}
-          </label>
+        <form onSubmit={(event) => event.preventDefault()}>
+          <label htmlFor="meme-template">Meme template</label>
           <input
-            id="Meme template"
+            id="meme-template"
             placeholder="type doge"
             onChange={(event) => setMemeGen(event.currentTarget.value)}
           />
+
           <br />
-          <br />
-          <br />
-          <label htmlFor="Top text">Upper Input</label>
+          <label htmlFor="Top-text">Top text</label>
           <input
             value={userInputUpper}
-            id="Top text"
+            id="Top-text"
             onChange={(event) => {
               setUserInputUpper(event.currentTarget.value);
             }}
           />
-          <br />
 
-          <br />
-          <label htmlFor="Bottom text">Lower Input</label>
+          <label htmlFor="Bottom-text">Bottom text</label>
           <input
             value={userInputLow}
-            id="Bottom text"
+            id="Bottom-text"
             onChange={(event) => setUserInputLow(event.currentTarget.value)}
           />
-          <br />
-          <br />
 
           <button>Generate meme</button>
-          <br />
-          <br />
-          {memeGen.length ? (
+
+          {memeGen && memeGen.length ? (
             <img
               htmlFor="generated-image"
-              style={{ height: '150px' }}
+              style={{ height: '250px' }}
               src={`${urlImages}${memeGen}/${userInputUpper.length > 0 ? userInputUpper : '_'}/${userInputLow.length > 0 ? userInputLow : '_'}.jpg`}
               alt="meme-image"
             />
           ) : (
             <img
-              htmlFor="preview-image"
+              data-test-id="meme-image"
               style={{ height: '150px' }}
               src="https://api.memegen.link/images/slap/example/preview.png?api_key=myapikey42&watermark=upleveled.com"
               alt="meme-image"
             />
           )}
         </form>
+
         <br />
-        <br />
-        <button>Download</button>
-        {/* <div>
-          Hello again
-          {image.map((imageItem) => {
-            return (
-              <div key={`my data item is-${imageItem.id}`}>
-                <img src={imageItem.blank} alt={imageItem.name} />
-              </div>
-            );
-          })}
-        </div> */}
+        <button class="bottom_button">Download</button>
       </div>
     </div>
   );
